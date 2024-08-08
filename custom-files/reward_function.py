6525d3f9a6532d6234d7fb60a4bc1a84a91246a1
rahulishu <rahulishu2010@gmail.com>
2024-08-08 18:16:34 -0400

    reward function update

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -12,15 +12,18 @@ def reward_function(params):
     # Calculate 3 marks that are farther and father away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    marker_3 = 0.35 * track_width
+    marker_4 = 0.5 * track_width
 
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 3
+        reward = 4 
     elif distance_from_center <= marker_2:
-        reward = 1.5
+        reward = 3
     elif distance_from_center <= marker_3:
-        reward = 0.15
+        reward = 2
+    elif distance_from_center <= marker_4:
+        reward = 1 
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
@@ -36,14 +39,19 @@ def reward_function(params):
     # read input parameter
     wp = params['closest_waypoints'][1]
     speed = params['speed']
-    if (wp in (list(range(10,22)))) or (wp in (list(range(130, 141)))):
-        if speed >= 1.5:
-            reward = 1e-3 # high speed at turning
-    elif (wp in (list(range(34,60)))) or (wp in (list(range(64, 82)))) or (wp in (list(range(91, 110)))):
-        if speed >= 1:
-            reward = 1e-3 # high speed at turning
-    else:
-        reward = reward + speed
-
+    if reward >= 1:
+        if (wp in (list(range(11,16)))) or (wp in (list(range(131, 136)))):
+            if speed >= 2.5:
+                reward += 1e-3 # high speed at turning
+            else:
+                reward += speed
+        elif (wp in (list(range(41,53)))) or (wp in (list(range(65, 77)))) or (wp in (list(range(93, 105)))):
+            if speed >= 2:
+                reward += 1e-3 # high speed at turning
+            else:
+                reward += speed
+        else:
+            reward += speed
+    
 
     return float(reward)
