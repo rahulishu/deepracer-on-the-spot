dd06eb19017193e2c6ea70a2c1139e68cbbd3741
rahulishu <rahulishu2010@gmail.com>
2024-09-19 23:00:01 -0400

    rssc y1 run

--- training-rahul.sh
+++ training-rahul.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./create-standard-instance.sh sandbox03-base-stack training-stack-rahul-RSSC-c1 360
+./create-standard-instance.sh sandbox03-base-stack training-stack-rahul-RSSC-y1 360
